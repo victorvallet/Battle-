d559ad715b5b00d6211dc539b4a29e5485258cbd
victorvallet <victorvallet8@gmail.com>
2020-05-28 17:51:36 +0200

    Passed our three tests

--- app.rb
+++ app.rb
@@ -9,6 +9,11 @@ get '/' do
   erb(:index)
 end
 
+post '/names' do
+    @name = params[:name]
+    erb(:play)
+end
+
 run! if app_file == $0
 end
 #
--- spec/features/app_spec.rb
+++ spec/features/app_spec.rb
@@ -3,7 +3,20 @@ require "capybara/rspec"
 feature "battle" do
   scenario "allows players to fill their names in a form" do
     visit("/")
+    fill_in('name', with: 'Victor')
     expect(find_field('name').value).to eq 'Victor'
+  end
+
+  scenario "allows players to submit their name" do
+    visit("/")
+    expect(find('input.submit-name')).not_to be_nil
+  end
+
+  scenario "allows the user to see its name" do
+    visit('/')
+    fill_in('name', with: 'Rich')
+    find('input.submit-name').click
+    expect(page).to have_content('Rich')
   end
 end
 
